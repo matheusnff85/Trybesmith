3d930a65211dfddf11184b94074b78ea5853d585
matheusnff85 <math.marinho154@gmail.com>
2022-09-13 18:24:33 +0000

    feat: implementa controllers e rotas para os products

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import ProductServices from '../services/products.services';
+
+export default class ProductController {
+  private productServices: ProductServices;
+
+  constructor() {
+    this.productServices = new ProductServices();
+  }
+
+  public getAll = async (req: Request, res: Response) => {
+    const result = await this.productServices.getAll();
+    return res.status(200).json(result);
+  };
+
+  public create = async (req: Request, res: Response) => {
+    const { name, amount } = req.body;
+    const newProduct = await this.productServices.create(name, amount);
+    return res.status(201).json(newProduct);
+  };
+}
