8d4f4916ac3fd6200b4a87d4d8ffe770bd94102e
matheusnff85 <math.marinho154@gmail.com>
2022-09-13 18:29:31 +0000

    feat: arquivo que gera o token jwt

---
+++ src/helpers/createToken.ts
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const secret: string = process.env.SECRET || 'secret';
+
+const createToken = (username: string, password: string) => {
+  const token = jwt.sign({ username, password }, secret);
+  return token;
+}
+
+export default createToken;
